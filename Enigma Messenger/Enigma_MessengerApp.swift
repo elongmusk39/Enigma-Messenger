46b33025cf3601ee9d3e3a8f4d44a7943bdeaf5c
long nguyen <elongmusk39@gmail.com>
2024-05-14 03:05:20 -0700

    Initial Commit

---
+++ Enigma Messenger/Enigma_MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enigma_MessengerApp.swift
+//  Enigma Messenger
+//
+//  Created by Long Nguyen on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Enigma_MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
