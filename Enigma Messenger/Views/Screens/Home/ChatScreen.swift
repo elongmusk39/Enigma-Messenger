63f996943c7c58bf42fcb0ea8df4445abc3ad08f
long nguyen <elongmusk39@gmail.com>
2024-10-22 02:37:10 -0700

    Showing all users in the system, user can pick any other user to start a chat.

---
+++ Enigma Messenger/Views/Screens/Home/ChatScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChatScreen.swift
+//  Enigma Messenger
+//
+//  Created by Long Nguyen on 10/21/24.
+//
+
+import SwiftUI
+
+struct ChatScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatScreen()
+}
