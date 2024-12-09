f4516332f88104ad733cc12bb945d453c8104502
elongmusk39 <elongmusk39@gmail.com>
2024-12-09 03:10:41 -0800

    Message Screen implemented, all UI tested.

---
+++ Enigma Messenger/APIServices/ServiceFetch.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceFetch.swift
+//  Enigma Messenger
+//
+//  Created by Long Nguyen on 12/8/24.
+//
+
+import Foundation
---
+++ Enigma Messenger/Views/Screens/TabScreen.swift
@@ -0,0 +1,8 @@
+//
+//  TabScreen.swift
+//  Enigma Messenger
+//
+//  Created by Long Nguyen on 12/8/24.
+//
+
+import Foundation
